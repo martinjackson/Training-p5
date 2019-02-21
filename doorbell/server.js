a79e171c21dbe12622770a8437a3760b3dfc2fa6
Martin A. Jackson <martin.a.jackson@gmail.com>
2019-02-21 14:05:12 -0600

    initial commit

---
+++ doorbell/server.js
@@ -0,0 +1,25 @@
+
+let express = require('express');
+let socket = require('socket.io')
+
+let app = express();
+let host = 3000
+let server = app.listen(host)
+
+app.use(express.static('public'));
+
+console.log("Socket server is running. localhost:" + host)
+
+let io = socket(server);
+
+io.sockets.on('connection', newConnection)
+
+function newConnection(socket){
+	console.log('connection:',	socket.id);
+	socket.on('mouse', mouseMsg);
+
+	function mouseMsg(data) {
+		// socket.broadcast.emit('mouse', data)
+		console.log(data)
+	}
+}
