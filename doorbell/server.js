4b23c60182a3661b19d0afb7d8eb31b71502ab9c
Martin Jackson <martin.a.jackson@gmail.com>
2019-02-25 14:42:04 -0600

    works with gpio light signaling correct mouse click

--- doorbell/server.js
+++ doorbell/server.js
@@ -1,6 +1,7 @@
 
 let express = require('express');
 let socket = require('socket.io')
+let rpio = require('rpio')
 
 let app = express();
 let host = 3000
@@ -10,16 +11,27 @@ app.use(express.static('public'));
 
 console.log("Socket server is running. localhost:" + host)
 
+// ---------------------------------------- 
+let light = 12;   // GPIO 18
+rpio.open(light, rpio.OUTPUT, rpio.LOW);
+
+
+// ---------------------------------------- 
 let io = socket(server);
 
 io.sockets.on('connection', newConnection)
 
-function newConnection(socket){
-	console.log('connection:',	socket.id);
-	socket.on('mouse', mouseMsg);
+function newConnection(socket) {
+   console.log('connection:', socket.id);
+   socket.on('mouse', mouseMsg);
 
-	function mouseMsg(data) {
-		// socket.broadcast.emit('mouse', data)
-		console.log(data)
-	}
+   function mouseMsg(data) {
+      // socket.broadcast.emit('mouse', data)
+
+      rpio.write(light, rpio.HIGH)                
+      console.log(data)
+      rpio.write(light, rpio.LOW)                
+   }
 }
+
+
