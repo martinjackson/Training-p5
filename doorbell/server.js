c40aee9d5f6e6680be51faa64215f2bc6d41cde3
Martin A. Jackson <martin.a.jackson@gmail.com>
2019-03-26 09:06:36 -0500

    working on serChild integration

--- doorbell/server.js
+++ doorbell/server.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let socket = require('socket.io')
 let rpio = require('rpio')
+const { fork } = require('child_process');
 
 let app = express();
 let host = 3000
@@ -11,27 +12,35 @@ app.use(express.static('public'));
 
 console.log("Socket server is running. localhost:" + host)
 
-// ---------------------------------------- 
+// ----------------------------------------
 let light = 12;   // GPIO 18
 rpio.open(light, rpio.OUTPUT, rpio.LOW);
 
-
-// ---------------------------------------- 
+// ----------------------------------------
 let io = socket(server);
 
 io.sockets.on('connection', newConnection)
 
 function newConnection(socket) {
    console.log('connection:', socket.id);
-   socket.on('mouse', mouseMsg);
+   const serChild = fork(`${__dirname}/ser-child.js`);
 
-   function mouseMsg(data) {
+   socket.on('mouse', (data) => {
       // socket.broadcast.emit('mouse', data)
 
-      rpio.write(light, rpio.HIGH)                
+      rpio.write(light, rpio.HIGH)
       console.log(data)
-      rpio.write(light, rpio.LOW)                
+      rpio.write(light, rpio.LOW)
    }
+
+   serChild.on('message', (m) => {
+      console.log('Socket server got message from serChild:', m);
+      socket.send('serial', m);
+    });
+
+    // Causes the child to print: CHILD got message: { hello: 'world' }
+    // serChild.send({ hello: 'world' });
+
 }
 
 
